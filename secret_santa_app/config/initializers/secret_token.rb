4ec99b6af096b087f9a15f57ad3a531437cf66bf
secret-santa <secret-santa@secretsanta-VirtualBox.(none)>
2012-12-24 02:16:10 +0000

    base project as promised. :)

---
+++ secret_santa_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecretSantaApp::Application.config.secret_token = 'f0d731d2cb0bc6596a2f03a10b7a88a116cc8f5bdc33a81fa6ba744560f3d28e241c72dc796979b034563214c26c8a738e755bb18a392cd092366c872509eaff'
